7dd34ff8deb811b465d558cac56581e1d7ce4408
Igor <riasik123400@gmail.com>
2019-01-12 01:06:16 +0200

    update 12.01

--- ITMED/script.js
+++ ITMED/script.js
@@ -1,25 +1,53 @@
-const inputElements = document.body.querySelector('.number');
+'use strict';
 
+let inputElements = document.querySelectorAll('.number');
+let formElement = document.querySelector('#form');
 
-inputElements.addEventListener("invalid", function () {
+(function validationForm() {
 
-    if (this.validity.valueMissing) {
-        this.setCustomValidity('Fill in the field!');
+    for (let i = 0; i < inputElements.length; i++) {
+        inputElements[i].addEventListener("invalid", function (event) {
+
+            let target = event.target;
+            if (target.validity.valueMissing) {
+                target.setCustomValidity('Field required for conscription!');
+            }
+        });
+
+        inputElements[i].addEventListener('input', function () {
+
+            let inputValue = inputElements[i].value;
+
+            if (inputElements[i].validity.patternMismatch) {
+                this.setCustomValidity(`You entered: "${inputValue}". Input only natural number, please!`);
+            } else {
+                this.setCustomValidity('');
+            }
+        });
     }
 
-});
+    formElement.addEventListener('submit', function (event) {
+        for(let j = 0; j < 6; j++){
+            let inputValue = [inputElements[j].value];
+            // console.log(inputValue); //6 input value
 
-inputElements.addEventListener('input', function () {
+            let newInputValueAll = [].push(inputValue);
+            console.log(newInputValueAll);
+        }
+        // let generalDiv = document.querySelector('.general');
+        // let newDiv = document.createElement("div");
+        // generalDiv.appendChild(newDiv);
+        //
+        // let newText = document.createElement('span');
+        //
+        // let text = document.createTextNode(`The value is in the order of growth: `);
+        // newText.appendChild(text);
+        // newDiv.appendChild(newText);
+        event.preventDefault();
+    });
 
-    let inputValue = inputElements.value;
+})();
 
-    if(inputElements.validity.patternMismatch){
-        this.setCustomValidity(`You entered: "${inputValue}". Input only natural number, please!`);
-    } else{
-        this.setCustomValidity('');
-    }
-
-});
 
 
 
